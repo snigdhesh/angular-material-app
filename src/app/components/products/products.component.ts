e4dca52d52c615e070670177cfe5fa8df5c3f631
Naga Vadlapudi <NagaSnigdhesh.Vadlapudi1@T-Mobile.com>
2021-08-13 09:56:06 -0400

    How to make use of services?

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit,OnDestroy {
   productName="A book";
   productList=["Book","Laptop","Box"];
-  isDisabled=true;
+  isEnabled=true;
+  
+  private productSubscription: Subscription;
 
-  constructor() { 
-    setTimeout(()=>{
-      this.isDisabled=false;
-    },5000);
+  constructor(private productService:ProductService) { 
   }
 
   ngOnInit() {
+    this.productSubscription=this.productService.productsUpdated.subscribe(()=>{
+      this.productList=this.productService.getProducts();
+    })
   }
-  addProduct(){
-    this.productList.push(this.productName);
+  addProduct(form){
+    if(form.valid)
+      this.productService.addProduct(form.value.productName);
   }
   removeProduct(productName:string){
     this.productList=this.productList.filter(product=>product!==productName);
   }
 
+  /**
+   * TODO: I think this method is not getting triggered, do a POC to see how this ngOnDestroy() method works.
+   */
+  ngOnDestroy(){
+    console.log("In ngOnDestroy method");
+    this.productSubscription.unsubscribe();
+  }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  
+  private products=['A Book'];
+  productsUpdated=new Subject();
+
+  constructor() { }
+
+  addProduct(productName:string){
+    this.products.push(productName);
+    this.productsUpdated.next();
+  }
+
+  getProducts(){
+    return [...this.products];
+  }
+
+  deleteProduct(productName:string){
+    this.products=this.products.filter(product=>product!==productName);
+    this.productsUpdated.next();
+  }
+}
