539616a15444de2e6269aa76069ff033e14c2bc4
naga vadlapudi <nagain2017@outlook.com>
2023-09-06 18:04:44 -0400

    Handled features: completedExercises, cancelExercises

--- src/app/components/training/current-training/current-training.component.ts
+++ src/app/components/training/current-training/current-training.component.ts
@@ -1,10 +1,8 @@
 import {
   Component,
   EventEmitter,
-  Input,
-  OnDestroy,
   OnInit,
-  Output,
+  Output
 } from "@angular/core";
 import { Subscription } from "rxjs";
 import { MatDialog } from "@angular/material/dialog";
@@ -20,13 +18,14 @@ import { Exercise } from "../exercise.model";
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
   timer: any;
-  @Input() exercise: Exercise;
+  runningExercise: Exercise;
 
   @Output() trainingExitEvent = new EventEmitter<void>();
   subscription: Subscription;
 
   constructor(
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private trainingService: TrainingService
   ) {}
 
   ngOnInit(): void{
@@ -34,14 +33,34 @@ export class CurrentTrainingComponent implements OnInit {
   }
 
   startOrResumeTimer() {
+    this.runningExercise=this.trainingService.getRunningExercise();
+    /**
+     * How step is calculated?
+     * Given exercise.duration = 30 sec
+     * Which means this 30sec is spread across 100%.
+     * So, for every 30/100 sec, we need to increse progress to 1 unit.
+     * For 100 units, it's 30 seconds. For 1 unit it's 0.3 seconds
+     * 
+     * Convert this 0.3 seconds to milliseconds. Because setInterval() method accepts time in milliseconds.
+     * To convert seconds to milliseconds, multilpy with 1000.
+     * so 0.3*1000
+     * 
+     * In total (exercise.duration/100)*1000 milliseconds.
+     * 
+     */
+    const step=this.trainingService.getRunningExercise().duration/100*1000; //Here duration is in seconds.
+
     this.timer = setInterval(() => {
-      this.progress = this.progress + 2;
-      if (this.progress >= 100) clearInterval(this.timer);
-    }, 100);
+      this.progress = this.progress + 1;
+      if (this.progress >= 100){
+        this.trainingService.completeExercise();
+        clearInterval(this.timer);//This default function will pause progress.
+      }
+    }, step); //Here step parameter is in milliseconds
   }
 
   onStop(): void {
-    clearInterval(this.timer); //This will pause progress.
+    clearInterval(this.timer); //This default function will pause progress.
 
     let testObj = {
       data: {
@@ -55,12 +74,17 @@ export class CurrentTrainingComponent implements OnInit {
 
     const dialogRef = this.dialog.open(StopTrainingComponent, testObj);
 
-    // When ever dialog box is closed, it returns an observerable. Hence we can subscribe to it
-    dialogRef.afterClosed().subscribe((result) => {
-      //console.log(result); -- To test what's coming in result of dialog box.
-      if (result)
-        this.trainingExitEvent.emit(); //This event is used in training.component.html
-      else this.startOrResumeTimer();
+    // When ever dialog box is closed, it returns an observerable. Hence we can subscribe to it.
+    dialogRef.afterClosed().subscribe((stopTraining) => {
+      //console.log(stopTraining); -- To test what's coming in 'stopTraining' of dialog box.
+      //"Do you want to stop traning?" result will be either 'true' or 'false' - we defined these boolen values in "stop-training.component.html"
+      //If "yes" is clicked on stop-training.component.html, it returns true
+      //If "no" is clicked on stop-training.component.html, it returns false
+      
+      if (stopTraining)//possible 'stopTraining' values are 'true' and 'false'
+        this.trainingService.cancelExercise(this.progress);
+      else 
+        this.startOrResumeTimer();
     });
   }
 }
--- src/app/components/training/training.service.ts
+++ src/app/components/training/training.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Exercise } from "./exercise.model";
-import {Subject} from 'rxjs';
+import { Subject } from "rxjs";
 
 @Injectable()
 export class TrainingService {
   constructor() {}
 
   private runningExercise: Exercise;
-  subject= new Subject<Exercise>();
-
+  private completedExercises: Exercise[] = [];
+  subject = new Subject<Exercise>();
 
   private availableExercises: Exercise[] = [
+    //All duration is in seconds
     { id: "crunches", name: "Crunches", duration: 30, calories: 8 },
-    { id: "touch-toes", name: "Touch toes", duration: 30, calories: 8 },
-    { id: "side-lunges", name: "Side lunges", duration: 30, calories: 8 },
-    { id: "burpees", name: "Burpees", duration: 30, calories: 8 },
+    { id: "touch-toes", name: "Touch toes", duration: 180, calories: 25 },
+    { id: "side-lunges", name: "Side lunges", duration: 60, calories: 35 },
+    { id: "burpees", name: "Burpees", duration: 10, calories: 100 },
   ];
 
   getAvailableExercises() {
@@ -22,7 +23,35 @@ export class TrainingService {
   }
 
   startTraining(selectedId: string) {
-    this.runningExercise= this.availableExercises.find(exercise => exercise.id === selectedId)
-    this.subject.next({...this.runningExercise});//This will emit an subject (which is equal to event) with a copy of this.runningExercise object
+    this.runningExercise = this.availableExercises.find(
+      (exercise) => exercise.id === selectedId
+    );
+    this.subject.next({ ...this.runningExercise }); //This will emit an subject (which is equal to event) with a copy of this.runningExercise object
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
+  }
+
+  completeExercise() {
+    this.completedExercises.push({
+      ...this.runningExercise,
+      date: new Date(),
+      state: "completed",
+    });
+    this.runningExercise = null;
+    this.subject.next(this.runningExercise);
+  }
+  
+  cancelExercise(progress: number) {
+    this.completedExercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress/100), //We overwrite duration here, because exercise is cancelled in middle
+      calories: this.runningExercise.duration * (progress/100), //We overwrite duration here, because exercise is cancelled in middle
+      date: new Date(),
+      state: "cancelled",
+    });
+    this.runningExercise = null;
+    this.subject.next(this.runningExercise);
   }
 }
